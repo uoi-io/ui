96defe2edda9437779789058ca6a13b5b80f2beb
goldyfruit <gaetan.trellu@incloudus.com>
2016-01-18 15:59:02 -0500

    [schema] Replace longtext field by text field because SQLAlchemy

--- sql/uoi.sql
+++ sql/uoi.sql
@@ -20,36 +20,36 @@ DROP TABLE IF EXISTS `Assets`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Assets` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hypervisor_id` int(11) DEFAULT NULL,
   `processor_id` int(11) DEFAULT NULL,
   `region_id` int(11) DEFAULT NULL,
   `role_id` int(11) DEFAULT NULL,
   `ipmi_id` int(11) DEFAULT NULL,
   `configuration_id` int(11) DEFAULT NULL,
+  `environment_id` int(11) DEFAULT NULL,
   `created` datetime NOT NULL,
   `mac` varchar(17) COLLATE utf8_unicode_ci NOT NULL,
   `uuid` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `vendor` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `status` int(11) NOT NULL,
-  `environment_id` int(11) DEFAULT NULL,
-  `hypervisor_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_7E7D78B81713EB65` (`mac`),
   UNIQUE KEY `UNIQ_7E7D78B837BAC19A` (`processor_id`),
   UNIQUE KEY `UNIQ_7E7D78B836ECCCDE` (`ipmi_id`),
   UNIQUE KEY `UNIQ_7E7D78B873F32DD8` (`configuration_id`),
+  KEY `IDX_7E7D78B84F2DF8A7` (`hypervisor_id`),
   KEY `IDX_7E7D78B898260155` (`region_id`),
   KEY `IDX_7E7D78B8D60322AC` (`role_id`),
   KEY `IDX_7E7D78B8903E3A94` (`environment_id`),
-  KEY `IDX_7E7D78B84F2DF8A7` (`hypervisor_id`),
+  CONSTRAINT `FK_7E7D78B8903E3A94` FOREIGN KEY (`environment_id`) REFERENCES `Environments` (`id`),
   CONSTRAINT `FK_7E7D78B836ECCCDE` FOREIGN KEY (`ipmi_id`) REFERENCES `Ipmi` (`id`),
   CONSTRAINT `FK_7E7D78B837BAC19A` FOREIGN KEY (`processor_id`) REFERENCES `Processors` (`id`),
   CONSTRAINT `FK_7E7D78B84F2DF8A7` FOREIGN KEY (`hypervisor_id`) REFERENCES `Hypervisors` (`id`),
   CONSTRAINT `FK_7E7D78B873F32DD8` FOREIGN KEY (`configuration_id`) REFERENCES `Configurations` (`id`),
-  CONSTRAINT `FK_7E7D78B8903E3A94` FOREIGN KEY (`environment_id`) REFERENCES `Environments` (`id`),
   CONSTRAINT `FK_7E7D78B898260155` FOREIGN KEY (`region_id`) REFERENCES `Regions` (`id`),
   CONSTRAINT `FK_7E7D78B8D60322AC` FOREIGN KEY (`role_id`) REFERENCES `Roles` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,17 +62,17 @@ DROP TABLE IF EXISTS `Commons`;
 CREATE TABLE `Commons` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `description` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `ntp` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `dns` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `status` tinyint(1) NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `ntp` text COLLATE utf8_unicode_ci NOT NULL,
+  `dns` text COLLATE utf8_unicode_ci NOT NULL,
   `domain` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `selinux` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `sshkey` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `sshkey` text COLLATE utf8_unicode_ci NOT NULL,
   `rootpwd` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `packages` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `packages` text COLLATE utf8_unicode_ci NOT NULL,
+  `status` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -90,10 +90,10 @@ CREATE TABLE `Configurations` (
   `gateway` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
   `domain` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `distribution` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `extraconf` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `extraconf` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_CE781201E551C011` (`hostname`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -105,14 +105,14 @@ DROP TABLE IF EXISTS `Environments`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Environments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `description` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `status` tinyint(1) NOT NULL,
   `common_id` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `status` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_D61973C98DBC56F7` (`common_id`),
   CONSTRAINT `FK_D61973C98DBC56F7` FOREIGN KEY (`common_id`) REFERENCES `Commons` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -126,12 +126,12 @@ CREATE TABLE `Hypervisors` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `address` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `description` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
   `status` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_B95208615E237E06` (`name`),
   UNIQUE KEY `UNIQ_B9520861D4E6F81` (`address`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -165,13 +165,13 @@ CREATE TABLE `Networks` (
   `interface` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `slaveof` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `master` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `options` longtext COLLATE utf8_unicode_ci,
+  `options` text COLLATE utf8_unicode_ci,
   `vlans` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `status` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_20CFD67D5DA1941` (`asset_id`),
   CONSTRAINT `FK_20CFD67D5DA1941` FOREIGN KEY (`asset_id`) REFERENCES `Assets` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -192,7 +192,7 @@ CREATE TABLE `Nics` (
   PRIMARY KEY (`id`),
   KEY `IDX_9A55185F5DA1941` (`asset_id`),
   CONSTRAINT `FK_9A55185F5DA1941` FOREIGN KEY (`asset_id`) REFERENCES `Assets` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -209,7 +209,7 @@ CREATE TABLE `Processors` (
   `model` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `freq` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -225,7 +225,7 @@ CREATE TABLE `Regions` (
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_6DDA406F5E237E06` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -239,11 +239,11 @@ CREATE TABLE `Roles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `created` datetime NOT NULL,
-  `description` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
   `status` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_77FF01C35E237E06` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -255,4 +255,4 @@ CREATE TABLE `Roles` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-01-09  9:00:44
+-- Dump completed on 2016-01-18 20:53:29
